a7646ec94b91e0477ff315abc5cb7e8e6f50f921
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-09-29 09:56:18 +0100

    load the database with fake data

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,38 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        $user->setName('Adebayo Ajibola');
+        $user->setEmailAddress('jibbol001@yahoo.com');
+        $user->setPassword('Password@12345!');
+
+        $user2 = new User();
+        $user2->setName('Adebayo Olaide');
+        $user2->setEmailAddress('jibbol001@yahoo.com');
+        $user2->setPassword('Password@12345!');
+
+        $user3 = new User();
+        $user3->setName('Adebayo Rilwan');
+        $user3->setEmailAddress('jibbol001@yahoo.com');
+        $user3->setPassword('Password@12345!');
+
+        $manager->persist($user);
+        $manager->persist($user2);
+        $manager->persist($user3);
 
         $manager->flush();
+        
+        $this->addReference('user_1', $user);
+        $this->addReference('user_2', $user2);
+        $this->addReference('user_3', $user3);
+
     }
 }
--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -2,14 +2,49 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Article;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use HashContext;
 
 class ArticleFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        $article = new Article();
+
+        $article->setTitle('Laravel vs Symfony');
+        $article->setShortDescription('This is an article that talks about how Laravel is compared to Symfony');
+        $article->setDateAdded('2022-09-27 12:33:44');
+        $article->setPicture('public/pictures/laravelandsymfony.png');
+        $article->setUser($this->getReference('user_2'));
+
+        $article2 = new Article();
+        $article2->setTitle('Laravel vs Symfony');
+        $article2->setShortDescription('This is an article that talks about how Laravel is compared to Symfony');
+        $article2->setDateAdded('2022-09-27 12:33:44');
+        $article2->setPicture('public/pictures/laravelandsymfony.png');
+        $article2->setUser($this->getReference('user_1'));
+
+        $article3 = new Article();
+        $article3->setTitle('Nodejs vs Nextjs');
+        $article3->setShortDescription('This is an article that talks about how Nodejs is compared to Nextjs');
+        $article3->setDateAdded('2022-09-27 12:33:44');
+        $article3->setPicture('public/pictures/laravelandsymfony.png');
+        $article3->setUser($this->getReference('user_2'));
+
+        $article4 = new Article();
+        $article4->setTitle('React vs Angular');
+        $article4->setShortDescription('This is an article that talks about how React is compared to Angular');
+        $article4->setDateAdded('2022-09-27 12:33:44');
+        $article4->setPicture('public/pictures/laravelandsymfony.png');
+        $article4->setUser($this->getReference('user_3'));
         
+        $manager->persist($article);
+        $manager->persist($article2);
+        $manager->persist($article3);
+        $manager->persist($article4);
 
         $manager->flush();
     }
